ec164f137ecccb2040fac9cf3cf5389cce89f11f
eekansh-M <eekansh.muppasani@outlook.com>
2024-09-10 20:27:08 -0400

    first commit

---
+++ src/Labs1.tsx
@@ -0,0 +1,11 @@
+export default function Labs(){
+    return(
+        <div>
+        <h1>Welcome to Web dev</h1>
+        <h2>Lab 1</h2>
+        <h3>Exercise 1.1</h3>
+        <h3>Exercise 1.2</h3>
+        </div>
+
+    )
+}
